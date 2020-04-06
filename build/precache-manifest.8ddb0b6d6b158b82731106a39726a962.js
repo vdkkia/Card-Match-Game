a0b45967342b999d0b9ca53ae01195cfcfcc1b26
vahid <vdkkia2004@yahoo.com>
2020-04-06 18:45:55 +0200

    added production

---
+++ build/precache-manifest.8ddb0b6d6b158b82731106a39726a962.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0607812ec9816fe47c8c25de64278443",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36228357d1d2fdbaa32c",
+    "url": "/static/css/main.a5a68f22.chunk.css"
+  },
+  {
+    "revision": "aa57d5b1c45f07ab2d64",
+    "url": "/static/js/2.1cab23a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1cab23a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36228357d1d2fdbaa32c",
+    "url": "/static/js/main.6582c19e.chunk.js"
+  },
+  {
+    "revision": "60c375f701e35a25ec1c",
+    "url": "/static/js/runtime-main.0e4125cc.js"
+  }
+]);
